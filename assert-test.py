98644f72431dfa4b6b65d16f966e32311f82737e
nduy <duyndx@gmail.com>
2022-06-11 19:31:43 +0100

    Simple  Factorial with assert

--- assert-test.py
+++ assert-test.py
@@ -4,6 +4,15 @@ Simple assert test
 """
 """ Original """
 
+def factorial(num):
+    assert type(num)==type(1)
+    assert num>=0
+
+    if (num==0):
+        return 1
+    else:
+        return num*factorial(num-1)
 
 if __name__=="__main__":
     print("!!")
+    print(factorial(30))
