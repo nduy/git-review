a910681b4730fef7103010c185f682163ed3c766
nduy <duyndx@gmail.com>
2022-06-12 10:43:16 +0100

    Add simple Uni test

--- Unit-test.py
+++ Unit-test.py
@@ -4,9 +4,9 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def factorial(num):
 #        assert type(num)==type(1)
-        self.assertIsInstance(num,int,"Is integer")
+        self.assertIsInstance(num,int,"Is integer") # Check if it is an interger
         assert num>=0
-        self.assertGreater(num,0,'Yes,greater')
+        self.assertGreater(num,0,'Yes,greater') # Check if it is a positive integer or Zero
         if (num==0):
             return 1
         else:
